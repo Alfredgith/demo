8ae9f9d7af4a893bc76690a3c13c215bd8aae8d1
Alfred Deshi <alfreddeshi4@example.com>
2026-01-24 01:47:35 +0100

    add hello world

--- src/main/java/com/example/backend/controller/HelloController.java
+++ src/main/java/com/example/backend/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*") // allow Android
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/")
 
     public  String helloBox(){
 
