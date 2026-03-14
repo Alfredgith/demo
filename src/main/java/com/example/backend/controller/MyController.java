f25de13a2a765434d738d0fa05bbd3b659e4375b
Alfred Deshi <alfreddeshi4@example.com>
2026-03-14 06:19:17 +0100

    first commit

---
+++ src/main/java/com/example/backend/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.example.backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class MyController {
+@GetMapping("/test")
+public String test() {
+    return "database connection test is success";
+}
+
+
+    
+}
