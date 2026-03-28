0c904b6da5252ba589bbf3b60d23802ea0ec7f54
Alfred Deshi <alfreddeshi4@example.com>
2026-03-28 02:41:21 +0100

    time

--- src/main/java/com/example/backend/controller/MyController.java
+++ src/main/java/com/example/backend/controller/MyController.java
@@ -1,10 +1,12 @@
 package com.example.backend.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
 
 
 
 @RestController
+@RequestMapping("/api")
 public class MyController {
 @GetMapping("/test")
 public String test() {
