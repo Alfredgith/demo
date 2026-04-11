d80a86bf09e353b5d6b5ca9b889bed71ebb4b043
Alfred Deshi <alfreddeshi4@example.com>
2026-04-11 14:43:00 +0100

    gat

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -3,6 +3,8 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class BackendApplication {
 
@@ -11,6 +13,7 @@ public class BackendApplication {
 	}
 
 }
+
 
 
 
