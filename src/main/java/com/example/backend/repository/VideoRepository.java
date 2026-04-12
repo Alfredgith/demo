a12936082f306e9ed130f1892a1939d7adadc0d1
Alfred Deshi <alfreddeshi4@example.com>
2026-04-12 13:15:23 +0100

    bop

---
+++ src/main/java/com/example/backend/repository/VideoRepository.java
@@ -0,0 +1,10 @@
+package com.example.backend.repository;
+import com.example.backend.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VideoRepository extends JpaRepository<Video, Long> {
+    Optional<Video> findByTitle(String title);
+}
+
